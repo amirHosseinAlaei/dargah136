5292a0612589ac2c04d28e030443cf27d5f40d3e
aminr hossein alaei <farbodshamloo04gmail.com>
2025-06-03 14:35:40 +0330

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,54 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import { fileURLToPath } from "url";
-
-// ساخت __dirname در محیط ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig(({ mode }) => {
-  // بارگذاری متغیرهای محیطی
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react(), tailwindcss(), basicSsl()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      basicSsl(),
+  VitePWA({
+  registerType: "autoUpdate",
+  includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+  manifest: {
+    name: "Bazresi App",
+    short_name: "Bazresi",
+    description: "بازرسی - Progressive Web App",
+    start_url: "/",
+    display: "standalone",
+    theme_color: "#ffffff",
+    background_color: "#ffffff", 
+    icons: [
+      {
+        src: "/dolatMan.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/dolatMan.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/dolatMan.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+}),
+    ],
     define: {
       "import.meta.env.APP_ENV": JSON.stringify(env.APP_ENV),
     },
     server: {
       host: "test.bazresi.ir",
+      // https: true,  <=== این خط رو حذف کن
     },
     resolve: {
       alias: {
